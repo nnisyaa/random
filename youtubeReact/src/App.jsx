bde92d9ea309ec9b20411f7856e5b0c646c54946
unknown <n.hasratova@mail.ru>
2024-05-10 14:09:48 +0400

    begin

---
+++ youtubeReact/src/App.jsx
@@ -0,0 +1,16 @@
+export default function App(){
+  return(
+    <>
+    <header>
+      <h1>university</h1>
+      <span>react</span>
+    </header>
+    <main>
+    <div><h1>Hello</h1></div>
+
+    </main>
+    </>
+  )
+}
+
+ 
